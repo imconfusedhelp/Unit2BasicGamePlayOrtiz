7ef18daecd4bced5227659d4b2b15df8463b619d
imconfusedhelp <demskillzdough721129@gmail.com>
2023-03-06 14:39:30 -0800

    Update 03/06/23 2:39pm
    Finished the challenge

--- Unit2BasicGamePlayOrtizP7/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Unit2BasicGamePlayOrtizP7/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,13 +10,10 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
-
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        SpawnRandomBall();
     }
 
     // Spawn random ball at random x position at top of play area
@@ -29,6 +26,7 @@ public class SpawnManagerX : MonoBehaviour
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        Invoke("SpawnRandomBall", Random.Range(3f, 5f));
     }
 
 }
