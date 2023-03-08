98ca49aa71faaf51f6c3fbcdb801fbb76eebb124
imconfusedhelp <demskillzdough721129@gmail.com>
2023-03-08 15:22:05 -0800

    Update 3:22pm 03/08/23
    Finished easy challenge

--- Unit2BasicGamePlayOrtizP7/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayOrtizP7/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
+
     public GameObject projectilePrefab;
 
     private float speed = 15f;
 
     public float xRange = 12f;
+    public float zRange = 3f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +27,26 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         // smoothly keeps the player from going out of bounds
-        if(transform.position.x < -xRange)
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if(transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        // smoothly keeps the player from going out of bounds
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
